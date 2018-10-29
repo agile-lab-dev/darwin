be855415879164698b8308d8db06df0aa1ce7584
Antonio Murgia <antonio.murgia@agilelab.it>
2018-10-29 11:18:06 +0100

    [#1] CrossPublish for 2.10 and 2.11
    Remove avro4s from dependencies since it is not available for 2.10
    Modules published now:
    darwin-core-common_2.10
    darwin-core-common_2.11
    darwin-core_2.10
    darwin-core_2.11
    darwin-hbase-connector_2.10
    darwin-hbase-connector_2.11
    darwin-postgres-connector_2.10
    darwin-postgres-connector_2.11
    darwin_2.10
    darwin_2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,3 +1,4 @@
+import Dependencies.avro4s
 import sbt._
 
 
@@ -20,7 +21,7 @@ object Dependencies {
 
   val core_deps = Seq(scalatest, avro, typesafe_config)
   val mock_app_dep = core_deps ++ Seq(avro4s, reflections)
-  val hbase_conn_dep = core_deps ++ Seq(avro4s, hbase_common, hbase_server, hadoop_common)
-  val postgres_conn_dep = core_deps ++ Seq(avro4s, postgres_conn)
+  val hbase_conn_dep = core_deps ++ Seq(hbase_common, hbase_server, hadoop_common)
+  val postgres_conn_dep = core_deps :+ postgres_conn
   val spark_app = mock_app_dep ++ Seq(spark_core, spark_sql, hbase_common)
 }
