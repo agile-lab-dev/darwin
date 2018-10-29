0279d0a660303b87f578a5e1cee972a948e54db9
Antonio Murgia <antonio.murgia@agilelab.it>
2018-10-29 12:40:55 +0100

    [#3] Add scala 2.12 version to crossbuild

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,5 +4,6 @@
 object Versions {
   val scala = "2.11.12"
   val scala_210 = "2.10.7"
-  val crossScalaVersions = Seq(scala_210, scala)
+  val scala_212 = "2.12.7"
+  val crossScalaVersions = Seq(scala_210, scala, scala_212)
 }
