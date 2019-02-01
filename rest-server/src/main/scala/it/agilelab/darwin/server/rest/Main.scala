d65cdd62e0bad777545813a82edfb2479a35f321
Andrea Fonti <andreafontict@gmail.com>
2019-02-01 18:44:50 +0100

    Fixed configuration loading

--- rest-server/src/main/scala/it/agilelab/darwin/server/rest/Main.scala
+++ rest-server/src/main/scala/it/agilelab/darwin/server/rest/Main.scala
@@ -3,27 +3,17 @@ package it.agilelab.darwin.server.rest
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
-import it.agilelab.darwin.connector.mock.MockConnector
-import it.agilelab.darwin.manager.LazyAvroSchemaManager
-import org.apache.avro.{Schema, SchemaBuilder}
+import it.agilelab.darwin.manager.AvroSchemaManagerFactory
 
 object Main {
-
-
-
 
   def main(args: Array[String]): Unit = {
 
     implicit val actorSystem: ActorSystem = ActorSystem()
     implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-
-    val connector = new MockConnector(ConfigFactory.load())
-    val schemaManager = new LazyAvroSchemaManager(connector)
-
-    val schema = SchemaBuilder.array().items(Schema.create(Schema.Type.INT))
-
-    schemaManager.registerAll(Seq(schema))
+    val config = ConfigFactory.load()
+    val schemaManager = AvroSchemaManagerFactory.initialize(config.getConfig("darwin"))
 
     HttpApp(DarwinService(schemaManager)).run()
 
