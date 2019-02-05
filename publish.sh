0d3d4113e54e66501e13f05a903e404ccb1ddf0d
Antonio Murgia <antonio.murgia@agilelab.it>
2019-02-06 00:23:35 +0100

    [#1.0.7] Make really publish rest-server for 2.12

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbt -mem 2048 clean scalastyle +publishSigned ++2.11.12 "project darwin-rest-server" publishSigned ++2.12.7 "project darwin-rest-server" publishSigned
+sbt -mem 2048 clean scalastyle +publishSigned ++2.11.12 "project darwin-rest-server" clean publishSigned ++2.12.7 "project darwin-rest-server" publishSigned
