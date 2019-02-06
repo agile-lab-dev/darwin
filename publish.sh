54ebc741b21539f2b8f5fe856fe88d9505ba60b3
Antonio Murgia <antonio.murgia@agilelab.it>
2019-02-06 17:02:53 +0100

    [#9] Run tests before publishing, setting proper version

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbt clean scalastyle +publishSigned ++2.11.12 "project darwin-rest-server" clean publishSigned ++2.12.7 "project darwin-rest-server" publishSigned
+sbt clean scalastyle +test +publishSigned ++2.11.12 "project darwin-rest-server" clean scalastyle test publishSigned ++2.12.7 "project darwin-rest-server" test publishSigned
--- version.sbt
+++ version.sbt
@@ -1,1 +1,2 @@
-version in ThisBuild := "0.0.1"
+version in ThisBuild := "1.0.8-SNAPSHOT"
+
