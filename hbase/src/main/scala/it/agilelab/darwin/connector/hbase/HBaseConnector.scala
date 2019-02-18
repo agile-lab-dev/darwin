6e83d5691d0e8ac8f8321382a9118f3a84e4a646
Antonio Murgia <antonio.murgia@agilelab.it>
2019-02-18 15:08:04 +0100

    [#37] Make HBaseConnectorCreator handle the connection

--- hbase/src/main/scala/it/agilelab/darwin/connector/hbase/HBaseConnector.scala
+++ hbase/src/main/scala/it/agilelab/darwin/connector/hbase/HBaseConnector.scala
@@ -1,103 +1,25 @@
 package it.agilelab.darwin.connector.hbase
 
-import com.typesafe.config.Config
-import it.agilelab.darwin.common.{using, Connector, Logging}
+import it.agilelab.darwin.common.{Connector, Logging, using}
 import org.apache.avro.Schema
 import org.apache.avro.Schema.Parser
-import org.apache.commons.io.IOUtils
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
 import org.apache.hadoop.hbase._
-import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Get, Put, Result}
-import org.apache.hadoop.hbase.security.User
+import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.hadoop.security.UserGroupInformation
 
 import scala.collection.JavaConverters._
 
+case class HBaseConnector(connection: Connection,
+                          namespace: String,
+                          tableName: String) extends Connector with Logging {
 
-object HBaseConnector extends Logging {
 
-  private var _instance: HBaseConnector = _
-
-  def instance(hbaseConfig: Config): HBaseConnector = {
-    synchronized {
-      if (_instance == null) {
-        log.debug("Initialization of HBase connector")
-        _instance = HBaseConnector(hbaseConfig)
-        log.debug("HBase connector initialized")
-      }
-    }
-    _instance
-  }
-}
-
-case class HBaseConnector(config: Config) extends Connector with Logging {
-
-  val DEFAULT_NAMESPACE: String = "AVRO"
-  val DEFAULT_TABLENAME: String = "SCHEMA_REPOSITORY"
-
-  val TABLE_NAME_STRING: String = if (config.hasPath(ConfigurationKeys.TABLE)) {
-    config.getString(ConfigurationKeys.TABLE)
-  } else {
-    DEFAULT_TABLENAME
-  }
-
-  val NAMESPACE_STRING: String = if (config.hasPath(ConfigurationKeys.NAMESPACE)) {
-    config.getString(ConfigurationKeys.NAMESPACE)
-  } else {
-    DEFAULT_NAMESPACE
-  }
-
-  lazy val TABLE_NAME: TableName = TableName.valueOf(Bytes.toBytes(NAMESPACE_STRING), Bytes.toBytes(TABLE_NAME_STRING))
+  val TABLE_NAME: TableName = TableName.valueOf(Bytes.toBytes(namespace), Bytes.toBytes(tableName))
 
   val CF: Array[Byte] = Bytes.toBytes("0")
   val QUALIFIER_SCHEMA: Array[Byte] = Bytes.toBytes("schema")
   val QUALIFIER_NAME: Array[Byte] = Bytes.toBytes("name")
   val QUALIFIER_NAMESPACE: Array[Byte] = Bytes.toBytes("namespace")
-
-  log.debug("Creating default HBaseConfiguration")
-  val configuration: Configuration = HBaseConfiguration.create()
-  log.debug("Created default HBaseConfiguration")
-
-  if (config.hasPath(ConfigurationKeys.CORE_SITE) && config.hasPath(ConfigurationKeys.HBASE_SITE)) {
-    log.debug(addResourceMessage(config.getString(ConfigurationKeys.CORE_SITE)))
-    configuration.addResource(new Path(config.getString(ConfigurationKeys.CORE_SITE)))
-    log.debug(addResourceMessage(config.getString(ConfigurationKeys.HBASE_SITE)))
-    configuration.addResource(new Path(config.getString(ConfigurationKeys.HBASE_SITE)))
-  }
-
-
-  private def addResourceMessage(s: String) = {
-    val ADDING_RESOURCE = "Adding resource: "
-    ADDING_RESOURCE + s
-  }
-
-  val connection: Connection = if (config.getBoolean(ConfigurationKeys.IS_SECURE)) {
-    log.debug(s"Calling UserGroupInformation.setConfiguration()")
-    UserGroupInformation.setConfiguration(configuration)
-
-    log.debug(s"Calling UserGroupInformation.loginUserFromKeytab(${config.getString(ConfigurationKeys.PRINCIPAL)}, " +
-      s"${config.getString(ConfigurationKeys.KEYTAB_PATH)})")
-    val ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(
-      config.getString(ConfigurationKeys.PRINCIPAL), config.getString(ConfigurationKeys.KEYTAB_PATH)
-    )
-    UserGroupInformation.setLoginUser(ugi)
-    val user = User.create(ugi)
-    log.trace(s"initialization of HBase connection with configuration:\n " +
-      s"${configuration.iterator().asScala.map { entry => entry.getKey -> entry.getValue }.mkString("\n")}")
-    ConnectionFactory.createConnection(configuration, user)
-  } else {
-    log.trace(s"initialization of HBase connection with configuration:\n " +
-      s"${configuration.iterator().asScala.map { entry => entry.getKey -> entry.getValue }.mkString("\n")}")
-    ConnectionFactory.createConnection(configuration)
-  }
-
-  log.debug("HBase connection initialized")
-  sys.addShutdownHook {
-    //  log.info(s"closing HBase connection pool")
-    IOUtils.closeQuietly(connection)
-  }
 
   //TODO this must be a def (a new Parser is created each time) because if the same Parser is used, it fails if you
   //TODO parse a class A and after it a class B that has a field of type A => ERROR: Can't redefine type A.
@@ -105,36 +27,39 @@ case class HBaseConnector(config: Config) extends Connector with Logging {
   private def parser: Parser = new Parser()
 
   override def fullLoad(): Seq[(Long, Schema)] = {
-    log.debug(s"loading all schemas from table $NAMESPACE_STRING:$TABLE_NAME_STRING")
-    val scanner: Iterable[Result] = connection.getTable(TABLE_NAME).getScanner(CF, QUALIFIER_SCHEMA).asScala
-    val schemas = scanner.map { result =>
-      val key = Bytes.toLong(result.getRow)
-      val value = Bytes.toString(result.getValue(CF, QUALIFIER_SCHEMA))
-      key -> parser.parse(value)
-    }.toSeq
+    log.debug(s"loading all schemas from table $namespace:$tableName")
+    val schemas = using(connection.getTable(TABLE_NAME).getScanner(CF, QUALIFIER_SCHEMA)) { scanner =>
+      scanner.asScala.map { result =>
+        val key = Bytes.toLong(result.getRow)
+        val value = Bytes.toString(result.getValue(CF, QUALIFIER_SCHEMA))
+        key -> parser.parse(value)
+      }.toSeq
+    }
     log.debug(s"${schemas.size} loaded from HBase")
     schemas
   }
 
   override def insert(schemas: Seq[(Long, Schema)]): Unit = {
-    log.debug(s"inserting ${schemas.size} schemas in HBase table $NAMESPACE_STRING:$TABLE_NAME_STRING")
-    val mutator = connection.getBufferedMutator(TABLE_NAME)
-    schemas.map { case (id, schema) =>
-      val put = new Put(Bytes.toBytes(id))
-      put.addColumn(CF, QUALIFIER_SCHEMA, Bytes.toBytes(schema.toString))
-      put.addColumn(CF, QUALIFIER_NAME, Bytes.toBytes(schema.getName))
-      put.addColumn(CF, QUALIFIER_NAMESPACE, Bytes.toBytes(schema.getNamespace))
-      put
-    }.foreach(mutator.mutate)
-    mutator.flush()
-    log.debug(s"insertion of schemas into $NAMESPACE_STRING:$TABLE_NAME_STRING successful")
+    log.debug(s"inserting ${schemas.size} schemas in HBase table $namespace:$tableName")
+
+    using(connection.getBufferedMutator(TABLE_NAME)) { mutator =>
+      schemas.map { case (id, schema) =>
+        val put = new Put(Bytes.toBytes(id))
+        put.addColumn(CF, QUALIFIER_SCHEMA, Bytes.toBytes(schema.toString))
+        put.addColumn(CF, QUALIFIER_NAME, Bytes.toBytes(schema.getName))
+        put.addColumn(CF, QUALIFIER_NAMESPACE, Bytes.toBytes(schema.getNamespace))
+        put
+      }.foreach(mutator.mutate)
+      mutator.flush()
+    }
+    log.debug(s"insertion of schemas into $namespace:$tableName successful")
   }
 
   override def createTable(): Unit = {
     using(connection.getAdmin) { admin =>
-      if (!admin.listNamespaceDescriptors().exists(_.getName == NAMESPACE_STRING)) {
-        log.info(s"Namespace $NAMESPACE_STRING does not exists, creating it")
-        admin.createNamespace(NamespaceDescriptor.create(NAMESPACE_STRING).build())
+      if (!admin.listNamespaceDescriptors().exists(_.getName == namespace)) {
+        log.info(s"Namespace $namespace does not exists, creating it")
+        admin.createNamespace(NamespaceDescriptor.create(namespace).build())
       }
       if (!tableExists()) {
         log.info(s"Table $TABLE_NAME does not exists, creating it")
@@ -151,12 +76,12 @@ case class HBaseConnector(config: Config) extends Connector with Logging {
 
   override def tableCreationHint(): String = {
     s"""To create namespace and table from an HBase shell issue:
-       |  create_namespace '$NAMESPACE_STRING'
-       |  create '$NAMESPACE_STRING:$TABLE_NAME_STRING', '0'""".stripMargin
+       |  create_namespace '$namespace'
+       |  create '$namespace:$tableName', '0'""".stripMargin
   }
 
   override def findSchema(id: Long): Option[Schema] = {
-    log.debug(s"loading a schema with id = $id from table $NAMESPACE_STRING:$TABLE_NAME_STRING")
+    log.debug(s"loading a schema with id = $id from table $namespace:$tableName")
     val get: Get = new Get(Bytes.toBytes(id))
     get.addColumn(CF, QUALIFIER_SCHEMA)
     val result: Result = connection.getTable(TABLE_NAME).get(get)
--- hbase/src/main/scala/it/agilelab/darwin/connector/hbase/HBaseConnectorCreator.scala
+++ hbase/src/main/scala/it/agilelab/darwin/connector/hbase/HBaseConnectorCreator.scala
@@ -2,13 +2,84 @@ package it.agilelab.darwin.connector.hbase
 
 import com.typesafe.config.Config
 import it.agilelab.darwin.common.{Connector, ConnectorCreator, Logging}
+import org.apache.commons.io.IOUtils
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory}
+import org.apache.hadoop.hbase.security.User
+import org.apache.hadoop.security.UserGroupInformation
+
+import scala.collection.JavaConverters._
 
 class HBaseConnectorCreator extends ConnectorCreator with Logging {
+
+  val DEFAULT_NAMESPACE: String = "AVRO"
+  val DEFAULT_TABLENAME: String = "SCHEMA_REPOSITORY"
+
   override def create(config: Config): Connector = {
     log.debug("creating the HBase connector")
-    val connector: Connector = HBaseConnector.instance(config)
+
+    val TABLE_NAME_STRING: String = if (config.hasPath(ConfigurationKeys.TABLE)) {
+      config.getString(ConfigurationKeys.TABLE)
+    } else {
+      DEFAULT_TABLENAME
+    }
+
+    val NAMESPACE_STRING: String = if (config.hasPath(ConfigurationKeys.NAMESPACE)) {
+      config.getString(ConfigurationKeys.NAMESPACE)
+    } else {
+      DEFAULT_NAMESPACE
+    }
+
+    val connector: Connector = HBaseConnector(getConnection(config), NAMESPACE_STRING, TABLE_NAME_STRING)
     log.debug("HBase connector created")
     connector
+  }
+
+  private def addResourceMessage(s: String) = {
+    val ADDING_RESOURCE = "Adding resource: "
+    ADDING_RESOURCE + s
+  }
+
+  private def getConnection(config: Config) = {
+    log.debug("Creating default HBaseConfiguration")
+    val configuration: Configuration = HBaseConfiguration.create()
+    log.debug("Created default HBaseConfiguration")
+
+    if (config.hasPath(ConfigurationKeys.CORE_SITE) && config.hasPath(ConfigurationKeys.HBASE_SITE)) {
+      log.debug(addResourceMessage(config.getString(ConfigurationKeys.CORE_SITE)))
+      configuration.addResource(new Path(config.getString(ConfigurationKeys.CORE_SITE)))
+      log.debug(addResourceMessage(config.getString(ConfigurationKeys.HBASE_SITE)))
+      configuration.addResource(new Path(config.getString(ConfigurationKeys.HBASE_SITE)))
+    }
+
+    val connection: Connection = if (config.getBoolean(ConfigurationKeys.IS_SECURE)) {
+      log.debug(s"Calling UserGroupInformation.setConfiguration()")
+      UserGroupInformation.setConfiguration(configuration)
+
+      log.debug(s"Calling UserGroupInformation.loginUserFromKeytab(${config.getString(ConfigurationKeys.PRINCIPAL)}, " +
+        s"${config.getString(ConfigurationKeys.KEYTAB_PATH)})")
+      val ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(
+        config.getString(ConfigurationKeys.PRINCIPAL), config.getString(ConfigurationKeys.KEYTAB_PATH)
+      )
+      UserGroupInformation.setLoginUser(ugi)
+      val user = User.create(ugi)
+      log.trace(s"initialization of HBase connection with configuration:\n " +
+        s"${configuration.iterator().asScala.map { entry => entry.getKey -> entry.getValue }.mkString("\n")}")
+      ConnectionFactory.createConnection(configuration, user)
+    } else {
+      log.trace(s"initialization of HBase connection with configuration:\n " +
+        s"${configuration.iterator().asScala.map { entry => entry.getKey -> entry.getValue }.mkString("\n")}")
+      ConnectionFactory.createConnection(configuration)
+    }
+
+    log.debug("HBase connection initialized")
+    sys.addShutdownHook {
+      //  log.info(s"closing HBase connection pool")
+      IOUtils.closeQuietly(connection)
+    }
+    connection
   }
 
   /**
