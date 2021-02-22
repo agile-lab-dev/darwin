0ceb1e2d6f0edf546efe19bd2b6cd437d6ac8551
Antonio Murgia <antonio.murgia@agilelab.it>
2021-02-22 12:18:26 +0100

    We've got secrets now

--- publish.sh
+++ publish.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-set -ex
-COMMAND=sbtn
-if ! command -v $COMMAND &> /dev/null
-then
-    echo "$COMMAND could not be found"
-    echo "using sbt"
-    COMMAND="sbt"
-else
-  if [ ! $TRAVIS ]; then
-    sbtn shutdown
-  fi
-fi
-$COMMAND "clean; scalastyle; +test; +publishSigned"
-$COMMAND "darwin-hbase2-connector/clean; darwin-hbase2-connector/scalastyle; +darwin-hbase2-connector/test; +darwin-hbase2-connector/publishSigned"
+export CI_RELEASE='clean;scalastyle;+test;+publishSigned;darwin-hbase2-connector/clean;darwin-hbase2-connector/scalastyle;+darwin-hbase2-connector/test;+darwin-hbase2-connector/publishSigned'
+export CI_SNAPSHOT_RELEASE='clean;scalastyle;+test;+publish;darwin-hbase2-connector/clean;darwin-hbase2-connector/scalastyle;+darwin-hbase2-connector/test;+darwin-hbase2-connector/publish'
+sbt ci-release
