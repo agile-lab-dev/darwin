914c37f1e417530b8164c0621e813f082f0c843a
Antonio Murgia <antonio.murgia@agilelab.it>
2021-02-23 10:59:14 +0100

    [#102] Update scala versions to 2.13.5 and 2.12.13 (#103)

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@
 object Versions {
   val scala_211          = "2.11.12"
   val scala_210          = "2.10.7"
-  val scala              = "2.12.12"
-  val scala_213          = "2.13.3"
+  val scala              = "2.12.13"
+  val scala_213          = "2.13.5"
   val crossScalaVersions = Seq(scala_210, scala_211, scala, scala_213)
 }
