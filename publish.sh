b97f8a0c7742970bcf0136ca9f50e23ee806c4bd
Antonio Murgia <antonio.murgia@agilelab.it>
2024-02-08 12:50:18 +0100

    Readd make

--- publish.sh
+++ publish.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export CI_RELEASE='+publishSigned;+darwin-hbase2-connector/publishSigned'
-export CI_SNAPSHOT_RELEASE='+publishSigned;+darwin-hbase2-connector/publishSigned'
+export CI_SNAPSHOT_RELEASE='+publish;+darwin-hbase2-connector/publish'
 sbt -v ci-release
-gpg --list-keys
